f91aebc6e346eeff630d2d35c6abd7fb445d670e
hajir3 <hajir3@gmail.com>
2022-07-18 17:06:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stuaid/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.stuaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.window.core.Version;
+
+import android.content.Intent;
+import android.graphics.Insets;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 6000;
+
+    
+
+/*
+    @SuppressWarnings("deprecation")
+    private void hideSystemBars() {
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
+    }*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
+        setContentView(R.layout.activity_main);
+/*
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+*/
+
+        //Hooks
+        View first,second,third,fourth,fifth,sixth,seventh;
+        TextView Tag,Logo;
+
+        //Animations
+        Animation topAnimation, bottomAnimation, middleAnimation;
+
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this,R.anim.middle_animation);
+
+        //Hooks
+        first = findViewById(R.id.first);
+        second = findViewById(R.id.second);
+        third = findViewById(R.id.third);
+        fourth = findViewById(R.id.fourth);
+        fifth = findViewById(R.id.fifth);
+        sixth = findViewById(R.id.sixth);
+        seventh = findViewById(R.id.seventh);
+
+        Logo = findViewById(R.id.Logo);
+        Tag = findViewById(R.id.Tag);
+
+
+        //setting Animations
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+        seventh.setAnimation(topAnimation);
+
+        Logo.setAnimation(middleAnimation);
+        Tag.setAnimation(bottomAnimation);
+
+
+        //Splash Screen
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent (MainActivity.this, secondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
---
+++ app/src/main/java/com/example/stuaid/secondActivity.java
@@ -0,0 +1,32 @@
+package com.example.stuaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class secondActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openthirdActivity();
+            }
+        });
+    }
+
+    public void openthirdActivity() {
+        Intent intent = new Intent(this, thirdActivity.class);
+        startActivity(intent);
+    }
+}
