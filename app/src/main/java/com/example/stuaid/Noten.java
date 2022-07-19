29c4ebce843bbb8291d4cce8c6e7f01acf26ad4a
hajir3 <hajir3@gmail.com>
2022-07-19 19:22:31 +0200

    fdsf

---
+++ app/src/main/java/com/example/stuaid/Noten.java
@@ -0,0 +1,14 @@
+package com.example.stuaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Noten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noten);
+    }
+}
