3177af949be2be0849a16527b4eca8d16a6977da
hajir3 <hajir3@gmail.com>
2022-07-19 19:13:59 +0200

    planer

---
+++ app/src/main/java/com/example/stuaid/Planer.java
@@ -0,0 +1,14 @@
+package com.example.stuaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Planer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planer);
+    }
+}
