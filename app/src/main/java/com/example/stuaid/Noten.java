c1cb1a0710b85d9b1b33b4ec1375998b94e7a662
hajir3 <hajir3@gmail.com>
2022-08-24 11:49:25 +0200

    Noten wurde wieder sichtbar gemacht

--- app/src/main/java/com/example/stuaid/Noten.java
+++ app/src/main/java/com/example/stuaid/Noten.java
@@ -2,17 +2,17 @@ package com.example.stuaid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import java.util.LinkedList;
-import java.util.List;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class Noten extends AppCompatActivity {
 
-    String []data = {"Hello","Hii", "Welcome"};
-    int counter = 0;
+
 
         public void openActivityHomepage() {
             Intent intent = new Intent(this,Homepage.class);
@@ -22,20 +22,18 @@ public class Noten extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_noten);
+            WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            WindowInsetsControllerCompat windowInsetsController =
+                    ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+            if (windowInsetsController == null) {
+                return;
+            }
+            windowInsetsController.setAppearanceLightNavigationBars(true);
+            windowInsetsController.setAppearanceLightStatusBars(true);
 
-            List<String> items = new LinkedList<>();
-            items.add("Code it");
 
-            RecyclerView recyclerView = findViewById(R.id.recyclerView);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-            NotenAdapter adapter = new NotenAdapter(items);
-            recyclerView.setAdapter(adapter);
-
-            findViewById(R.id.add).setOnClickListener(view -> {
-                items.add(data[counter % 3]);
-                counter++;
-                adapter.notifyItemInserted(items.size() - 1);
-            });
 
             Button homeButton = findViewById(R.id.home_button);
             homeButton.setOnClickListener(v -> openActivityHomepage());
