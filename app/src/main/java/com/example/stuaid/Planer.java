698a0d9634d9e58755925997fca571d36998697e
hajir3 <hajir3@gmail.com>
2022-08-31 20:23:27 +0200

    Activity Planer repariert und wieder aufrufbar. Fehler lag im /@Override                                                   protected void onCreate(Bundle savedInstanceState){       super.onCreate(savedInstanceState);                       setContentView(R.layout.activity_todo); das soltle jedoch heissen: setContentView(R.layout.activity_planer) das aufrufbare content war auf todo.xml getellt

--- app/src/main/java/com/example/stuaid/Planer.java
+++ app/src/main/java/com/example/stuaid/Planer.java
@@ -1,25 +1,17 @@
 package com.example.stuaid;
 
-import static com.example.stuaid.R.id.add_item;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class Planer extends AppCompatActivity {
 
-    private ArrayList<String> items;
-    private ArrayAdapter<String> itemsAdapter;
-    private ListView listView;
-    @SuppressLint("WrongViewCast")
-    private Button button = findViewById(add_item);
 
     public void openActivityHomepage() {
         Intent intent = new Intent(this,Homepage.class);
@@ -30,49 +22,20 @@ public class Planer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planer);
 
-        Button homeButton = findViewById(R.id.home_button);
-        homeButton.setOnClickListener(v ->openActivityHomepage());
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+        windowInsetsController.setAppearanceLightNavigationBars(true);
+        windowInsetsController.setAppearanceLightStatusBars(true);
 
-/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_planer);
-
-        listView = findViewById(R.id.listView);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addItem(view);
-
-            }
-        });
-        items = new ArrayList<>();
-        itemsAdapter   = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-        listView.setAdapter(itemsAdapter);
-        setupListViewListener();
+        Button homeButton3 = findViewById(R.id.home_button3);
+        homeButton3.setOnClickListener(v ->openActivityHomepage());
     }
-
-    private void setupListViewListener() {
-        return listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Context context = getApplicationContext();
-                Toast.makeText(context, resid;"Item removed";
-                items.remove(i);
-                itemsAdapter.notifyDataSetChanged();
-                return true;
-
-            }
-
-        });
-    }
-
-    private void addItem(View view) {
-    }
-
-
-     */
-}}
+}
 
 
--- app/src/main/java/com/example/stuaid/ToDo.java
+++ app/src/main/java/com/example/stuaid/ToDo.java
@@ -2,11 +2,17 @@ package com.example.stuaid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 public class ToDo extends AppCompatActivity {
+
+
 
 
     public void openActivityHomepage() {
@@ -16,10 +22,23 @@ public class ToDo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_to_do;
+        setContentView(R.layout.activity_to_do);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+        windowInsetsController.setAppearanceLightNavigationBars(true);
+        windowInsetsController.setAppearanceLightStatusBars(true);
 
         Button homeButton2 = findViewById(R.id.home_button2);
         homeButton2.setOnClickListener(v ->openActivityHomepage());
+
+
 
     }
 }
