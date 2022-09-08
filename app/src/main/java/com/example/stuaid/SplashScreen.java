7e1b1b3a578510d4d75ace5fb6f83d6a0b8425ee
cutie4pie <michelle.schneeberger@hotmail.com>
2022-09-08 13:46:55 +0200

    Splashscreen

--- app/src/main/java/com/example/stuaid/SplashScreen.java
+++ app/src/main/java/com/example/stuaid/SplashScreen.java
@@ -21,7 +21,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
