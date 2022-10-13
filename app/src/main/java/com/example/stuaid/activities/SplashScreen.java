3bc8e36789b4f46e32d528a2d235bc279e207e7d
hajir3 <hajir3@gmail.com>
2022-10-13 15:54:57 +0200

    Tasks TODO

---
+++ app/src/main/java/com/example/stuaid/activities/SplashScreen.java
@@ -0,0 +1,84 @@
+package com.example.stuaid.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+
+import com.example.stuaid.R;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    private static final int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
+        setContentView(R.layout.activity_splashscreen);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        //Hooks
+        View first,second,third,fourth,fifth,sixth,seventh,eight,ninth;
+        TextView Tag,Logo;
+
+        //Animations
+        Animation topAnimation, bottomAnimation, middleAnimation;
+
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this,R.anim.middle_animation);
+
+        //Hooks
+        first = findViewById(R.id.first);
+        second = findViewById(R.id.second);
+        third = findViewById(R.id.third);
+        fourth = findViewById(R.id.fourth);
+        fifth = findViewById(R.id.fifth);
+        sixth = findViewById(R.id.sixth);
+        seventh = findViewById(R.id.seventh);
+        eight = findViewById(R.id.eighth);
+        ninth = findViewById(R.id.ninth);
+
+        Logo = findViewById(R.id.Logo);
+        Tag = findViewById(R.id.Tag);
+
+
+        //setting Animations
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+        seventh.setAnimation(topAnimation);
+        eight.setAnimation(topAnimation);
+        ninth.setAnimation(topAnimation);
+
+        Logo.setAnimation(middleAnimation);
+        Tag.setAnimation(bottomAnimation);
+
+
+        //Splash Screen
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent (SplashScreen.this, Homepage.class);
+            startActivity(intent);
+            finish();
+        },SPLASH_TIME_OUT);
+    }
+}
---
+++ app/src/main/java/com/example/stuaid/activities/Task.java
@@ -0,0 +1,64 @@
+package com.example.stuaid.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+import com.example.stuaid.R;
+import com.example.stuaid.activities.Exams;
+import com.example.stuaid.activities.Grades;
+import com.example.stuaid.activities.Homepage;
+
+
+public class Task extends AppCompatActivity {
+
+
+    public void openActivityHomepage() {
+        Intent intent = new Intent(this, Homepage.class);
+        startActivity(intent);
+        }
+    public void openActivityNoten(){
+        Intent intent = new Intent(this, Grades.class);
+        startActivity(intent);
+        }
+    public void openActivityPlaner() {
+        Intent intent = new Intent(this, Exams.class);
+        startActivity(intent);
+        }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+        windowInsetsController.setAppearanceLightNavigationBars(true);
+        windowInsetsController.setAppearanceLightStatusBars(true);
+
+        Button homeButtonTodo = findViewById(R.id.home_button_todo);
+        homeButtonTodo.setOnClickListener(v ->openActivityHomepage());
+
+        Button planerButtonTodo = findViewById(R.id.planer_button_todo);
+        planerButtonTodo.setOnClickListener(v -> openActivityPlaner());
+
+        Button notenButtonTodo = findViewById(R.id.noten_button_todo);
+        notenButtonTodo.setOnClickListener(v -> openActivityNoten());
+
+    }
+}
+
+
